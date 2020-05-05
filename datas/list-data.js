555036ecc041392fc95db91aeaba518d4c3b57ee
Cently1 <1511384361@qq.com>
2020-05-05 17:52:05 +0800

    mini Program

---
+++ datas/list-data.js
@@ -0,0 +1,161 @@
+let list_data = [
+  {
+    date: 'may 22 2020',
+    title: '教育理念',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20190530160931.jpg',
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    detail_content: '元旦喜悦的氛围笼罩在城市的上空，我和小宇一起走在环形花坛的路上，小宇带着一个医疗口罩，边走边和我说：我书包还有好多，要不你也拿点带上，我笑着摇摇头说：“还是别了，带着这玩意儿一说话，镜片上一层雾，呼吸老费劲儿了。”殊不知那时的新型肺炎已经开始在武汉这座城市蔓延，我傻傻很天真的不觉于然。每天照常上下班，在路上偶遇一位阿姨不会开共享单车的锁，我热情的凑上去帮人把锁开了。那时，尽管寒风肆虐的在搜刮我身体，但心里很暖，我完全察觉不到这个看不到的杀手就潜伏在我四周。',
+    headImgSrc: '/images/detail/carousel/02.jpg',
+    author: '钢铁侠 ',
+    dataTime: '24time',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '钢铁战队。。',
+    music: {
+      dataUrl: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/%E6%9D%A8%E5%AE%97%E7%BA%AC%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD%20(%E5%8D%95%E6%9B%B2).mp3', // 音乐链接
+      title: '《一次就好》杨宗纬',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    
+    date: 'may 10 2020',
+    title: '尚硅谷',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/1245.jpg',
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    detail_content: '每天都在重复的奔向上班的那条路，寒冷的冬天，我是顾不上路边的风景，双腿在开合间以指数倍在增快，什么花花草草，车马行人直接3倍速快进，工作后的我，多少是变得有些麻木，什么工作顺利、生活顺心、学业顺意完全是不存在的。手里是干不完的工作，耳朵里是领导的斥责声，眼里是看不见尽头的未来。不知从何时起，我也开始变得眼里没有了光，曾试图放慢生活的节奏，以为这样就能让自己在色彩斑斓的城市中，找到自己的栖身之地，但事与愿违，直到那天我看见了一双眼睛——那女孩的眼睛。',
+    headImgSrc: '/images/detail/carousel/01.jpg',
+    author: '美国队长',
+    dataTime: '24time',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+
+    love_count: 88,
+    attention_count: 66,
+    detail: '有魅力的老男人。',
+    music: {
+      dataUrl: 'http://www.ytmp3.cn/down/50395.mp3', // 音乐链接
+      title: '一路向北',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 1
+  },
+  {
+
+    date: 'may 19 2020',
+    title: '学科介绍',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20190427172329.jpg',
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    detail_content: '尚硅谷现开设JavaEE+大数据、HTML5前端+全栈、大数据+机器学习、Python+人工智能、Android+HTML5混合开发等多门学科；同时，通过视频分享、谷粒学院在线课堂、直播课堂等多种方式，满足了全国编程爱好者对多样化学习场景的需求。目前，尚硅谷“谷粉”人数已超500万，面授班学员绝大多数都在北上广深等一线城市高薪就业，就业薪资更是屡创新高！',
+    headImgSrc: '/images/detail/carousel/03.jpg',
+    author: '绿巨人',
+    dataTime: '24time',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '巨无霸教授。',
+    music: {
+      dataUrl: 'http://www.ytmp3.cn/down/50355.mp3', // 音乐链接
+      title: '听海',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 2
+  },
+  {
+    date: '2010/3/15 下午 4:30:35',
+    title: '贾静雯简介',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20190530160931.jpg',
+    detail_content: '贾静雯（Alyssa Chia），1974年10月7日出生于台湾省台北市，祖籍天津市，华语影视女演员、节目主持人。1990年，贾静雯因接拍爱之味妞妞甜八宝广告而出道演艺圈；同年，她还出演了个人的首部电视剧《佳家福》。1994年，贾静雯开始担任一系列儿童节目的主持人。1997年，她凭借家庭剧《四千金》获得台湾电视金钟奖最佳新人奖[1]  。2000年，贾静雯主演的古装剧《飞龙在天》获得了台湾电视剧年度收视冠军。',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+
+    love_count: 92,
+    headImgSrc: '/images/detail/list/j2.jpg',
+    author: '新华社',
+    attention_count: 88,
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46101.mp3', // 音乐链接
+      title: 'Sunset Jesus-Avicii',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 3
+  },
+  {
+    date: '2020/3/17 下午3:30:35',
+    title: '贾静雯作品展',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20190530160931.jpg',
+    detail_content: '2001年，贾静雯将工作重心转向内地，并主演了传奇剧《大汉天子》。2002年，她凭借武侠剧《倚天屠龙记》在内地赢得更高关注度[2]  。2003年，贾静雯获得FHM全球百大性感美女亚洲区冠军。2006年，他主演了现代剧《悲伤时唱首歌》。此后几年，贾静雯相继出演了传奇剧《太平公主秘史》、剧情片《不能说的夏天》等影视作品',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+
+    love_count: 88,
+    attention_count: 66,
+    headImgSrc: '/images/detail/list/j3.jpg',
+    author: '新华社',
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46102.mp3', // 音乐链接
+      title: '汪峰 - 儿时',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 4
+  },
+  {
+
+    date: 'sep 1 2020',
+    title: '娱乐新闻',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20190530160931.jpg',
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    detail_content: '2014年，贾静雯与小自己九岁的台湾演员修杰楷相恋[41]  。2015年5月5日，贾静雯的男友修杰楷在微博宣布了贾静雯已身怀有孕的消息，并发出了他与贾静雯和梧桐妹的两张合照，随后，贾静雯转发了此条微博并希望得到大家的祝福[42]  ；同年8月7日，贾静雯以剖宫产方式生下了与修杰楷的第一个女儿“咘咘”，而此前两人已正式注册结婚[43]  。',
+    headImgSrc: '/images/detail/list/j4.jpg',
+    author: '李白3',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+
+    love_count: 88,
+    attention_count: 66,
+    detail: '女神。。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46110.mp3', // 音乐链接
+      title: '曲婉婷 - 我的歌声里-(电视剧《在线爱》主题曲)',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 5
+  },
+  {
+
+    date: 'sep 20 2010',
+    title: '职场篇；剑未佩妥，江湖受挫',
+    detail_img: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20190530160931.jpg',
+    avatar: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200425104256.jpg',
+    detail_content: '2012年，贾静雯担任公益大使，发起为弱势儿童和青少年课后照护计划，并为此次活动献唱了个人单曲《许一个愿望》。2014年，贾静雯参加了最终梦想年度时尚魅力女性颁奖盛典，并在典礼上获得了亲情天使奖[50] ',
+    headImgSrc: '/images/detail/list/j6.jpg',
+    author: '新华社',
+    detail_love_image1: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505102646.png',
+    detail_love_image2: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101114.png',
+    detail_love_image3: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/img/20200505101111.png',
+
+    love_count: 88,
+    attention_count: 66,
+    detail: '女神2。。。',
+    music: {
+      dataUrl: 'https://halen-blog-1252760404.cos.ap-chengdu.myqcloud.com/%E6%9D%A8%E5%AE%97%E7%BA%AC%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD%20(%E5%8D%95%E6%9B%B2).mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 6
+  },
+];
+
+module.exports = {list_data};
